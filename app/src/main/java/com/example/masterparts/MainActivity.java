27f25b6a70b700040ee4bfe117524d50b54032f5
DILEESHA <dileeshanawarathna18@gmail.com>
2021-09-19 00:30:55 +0530

    redirect path created

--- app/src/main/java/com/example/masterparts/MainActivity.java
+++ app/src/main/java/com/example/masterparts/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.masterparts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public CardView card1,card2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        card1 = (CardView) findViewById(R.id.c1);
+        card2 = (CardView) findViewById(R.id.c2);
+
+        card1.setOnClickListener(this);
+        card2.setOnClickListener(this);
+
+        }
+
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i;
+
+        switch (view.getId()) {
+            case R.id.c1 :
+                i = new Intent(this,AddActivity.class);
+                startActivity(i);
+                break;
+        }
+
+
     }
 }
+
+
+
