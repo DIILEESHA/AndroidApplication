2fbd360534c7876742b76ea65720a4a61dfa7e53
DILEESHA <dileeshanawarathna18@gmail.com>
2021-09-20 19:09:00 +0530

    insert method completed

--- app/src/main/java/com/example/masterparts/AddActivity.java
+++ app/src/main/java/com/example/masterparts/AddActivity.java
@@ -3,7 +3,9 @@ package com.example.masterparts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,68 +17,115 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class AddActivity extends AppCompatActivity {
 
-    private EditText mTitle , mDesc;
-    private Button mSaveBtn , mShowBtn;
+    //view
+    EditText mTitleEt, mDescriptionEt,mBrandEt,mEnginecEt,mFueluseEt,mAddressEt;
+    Button mSaveBtn;
 
-    private FirebaseFirestore db;
+    //dialog
+    ProgressDialog pd;
+
+    //firebase instance
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+//
+//        //action bar and title
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setTitle("Add Data");
 
-        mTitle = findViewById(R.id.edit_title);
-        mDesc = findViewById(R.id.edit_desc);
-        mSaveBtn = findViewById(R.id.save_btn);
-        mShowBtn = findViewById(R.id.showall_btn);
+        //initalize values
+        mTitleEt = findViewById(R.id.titleEt);
+        mDescriptionEt = findViewById(R.id.descriptionEt);
+        mBrandEt = findViewById(R.id.brandEt);
+        mEnginecEt = findViewById(R.id.enginecEt);
+        mFueluseEt = findViewById(R.id.fueluseEt);
+        mAddressEt = findViewById(R.id.addressEt);
+        mSaveBtn = findViewById(R.id.saveBtn);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //progress
+        pd = new ProgressDialog(this);
 
+        //firestore
+        db = FirebaseFirestore.getInstance();
+
+        //click button to send the data
         mSaveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String title = mTitle.getText().toString();
-                String desc = mDesc.getText().toString();
-                String id = UUID.randomUUID().toString();
+            public void onClick(View view) {
+                //input data
+                String title = mTitleEt.getText().toString().trim();
+                String description = mDescriptionEt.getText().toString().trim();
+                String brand = mBrandEt.getText().toString().trim();
+                String enginec = mEnginecEt.getText().toString().trim();
+                String fueluse = mFueluseEt.getText().toString().trim();
+                String address = mAddressEt.getText().toString().trim();
 
-                saveToFireStore(id, title, desc);
+                //function call to upload data
+                uploadData(title, description ,brand ,enginec ,fueluse ,address);
+
+                if(TextUtils.isEmpty(title)){
+                mTitleEt.setError("fields are empty");
+                return;
+                }
+
             }
         });
+
     }
-            private void saveToFireStore(String id, String title, String desc) {
 
-            if(title.isEmpty() && !desc.isEmpty()) {
-                HashMap<String , Object> map = new HashMap<>();
-                map.put("id" , id);
-                map.put("title" , title);
-                map.put("desc" , desc);
+//    private void uploadData(String title, String description, String brand, String enginec, String fueluse, String address) {
 
 
+    private void uploadData(String title, String description ,String brand, String enginec, String fueluse, String address) {
+        //set title
+        pd.setTitle("Processing");
+        //when the user click save btn
+        pd.show();
+        //random id to each data to be stored
+        String id = UUID.randomUUID().toString();
 
-                db.collection("Documents").document(id).set(map)
-                     .addOnCompleteListener(new OnCompleteListener<Void>() {
-                         @Override
-                         public void onComplete(@NonNull Task<Void> task) {
-                             if (task.isSuccessful()) {
-                                 Toast.makeText(AddActivity.this, "Data Saved !!", Toast.LENGTH_SHORT).show();
-                             }
-                         }
-                     }).addOnFailureListener(new OnFailureListener() {
+        Map<String, Object> doc = new HashMap<>();
+        doc.put("id", id);
+        doc.put("title", title);
+        doc.put("description", description);
+        doc.put("brand", brand);
+        doc.put("enginec",enginec);
+        doc.put("fueluse",fueluse);
+        doc.put("address",address);
+
+
+
+        //add this data
+        db.collection("Documents").document(id).set(doc)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //this will be called data added successfully
+
+                        pd.dismiss();
+                        Toast.makeText(AddActivity.this, "Saved Successfully", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(AddActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        //unsuccessfully
+
+                        //show error
+
+                        pd.dismiss();
+                        Toast.makeText(AddActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
                     }
                 });
-
-
-            }else
-              Toast.makeText(this, "Fields are empty", Toast.LENGTH_SHORT).show();
-
-
-        }
-
     }
+}
