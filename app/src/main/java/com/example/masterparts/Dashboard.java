a177a2a102922d15a37713cd12bcc9b6c416e0f7
DILEESHA <dileeshanawarathna18@gmail.com>
2021-09-24 18:10:08 +0530

    change dashboard view

--- app/src/main/java/com/example/masterparts/Dashboard.java
+++ app/src/main/java/com/example/masterparts/Dashboard.java
@@ -6,71 +6,94 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 
-public class Dashboard extends AppCompatActivity implements View.OnClickListener{
+public class Dashboard extends AppCompatActivity {
 
-    public CardView card1,card2,card3,card4;
+    public CardView main1, main2, main3, main4, main5, main6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        card1 = (CardView) findViewById(R.id.c1);
-        card2 = (CardView) findViewById(R.id.c2);
-        card3 = (CardView) findViewById(R.id.c3);
-        card4 = (CardView) findViewById(R.id.c4);
+        main1 = (CardView) findViewById(R.id.main1);
+        main2 = (CardView) findViewById(R.id.main2);
+        main3 = (CardView) findViewById(R.id.main3);
+        main4 = (CardView) findViewById(R.id.main4);
+        main5 = (CardView) findViewById(R.id.main5);
+        main6 = (CardView) findViewById(R.id.main6);
 
-        card1.setOnClickListener(this);
-        card2.setOnClickListener(this);
-        card3.setOnClickListener(this);
-        card4.setOnClickListener(this);
-
-        }
-
-
-    @Override
-    public void onClick(View view) {
-
-        Intent i;
-
-        switch (view.getId()) {
-            case R.id.c1 :
-                i = new Intent(this,AddActivity.class);
-                startActivity(i);
-                break;
-
-            case R.id.c2:
-                i = new Intent(this,IsuruActivity.class);
-                startActivity(i);
-                break;
-
-            case R.id.c3:
-                i = new Intent(this,SenuraActivity.class);
-                startActivity(i);
-                break;
-
-
-            case R.id.c4:
-                i = new Intent(this,JnithaActivity.class);
-                startActivity(i);
-                break;
-        }
-
-
-    }
-
-    public void logout(View view) {
-
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(getApplicationContext(),Login.class));
-            finish();
-
+        main1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Dashboard.this, AddActivity.class));
+                finish();
+            }
+        });
+        main2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Dashboard.this, IsuruActivity.class));
+                finish();
+            }
+        });
     }
 }
 
-
-
+//        main1.setOnClickListener(this);
+//        main2.setOnClickListener(this);
+//        main3.setOnClickListener(this);
+//        main4.setOnClickListener(this);
+//        main5.setOnClickListener(this);
+//        main6.setOnClickListener(this);
+//
+//        }
+//
+//
+//    @Override
+//    public void onClick(View view) {
+//
+//        Intent i;
+//
+//        switch (view.getId()) {
+//            case R.id.main1 :
+//                i = new Intent(this,AddActivity.class);
+//                startActivity(i);
+//                break;
+//
+//            case R.id.main2:
+//                i = new Intent(this,IsuruActivity.class);
+//                startActivity(i);
+//                break;
+//            Toast("clicked");
+//
+//            case R.id.c3:
+//                i = new Intent(this,SenuraActivity.class);
+//                startActivity(i);
+//                break;
+//
+//
+//            case R.id.c4:
+//                i = new Intent(this,JnithaActivity.class);
+//                startActivity(i);
+//                break;
+//        }
+//
+//
+//    }
+//
+//    public void logout(View view) {
+//
+//            FirebaseAuth.getInstance().signOut();
+//            startActivity(new Intent(getApplicationContext(),Login.class));
+//            finish();
+//
+//    }
+//}
+//
+//
+//
